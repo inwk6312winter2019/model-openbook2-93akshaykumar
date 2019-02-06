4b88767225f3a9bb88fd37c0c00a6ba9389cd9ba
akshaykumar <akshayk@dal.ca>
2019-02-06 19:58:17 -0400

    Removed extra code and scafffolding values

--- task2.py
+++ task2.py
@@ -7,6 +7,15 @@ def read_csv(fout):
 
     return datalist
 
+
+def traverse(line,street_datalist,checkvalue):
+    counter=0
+    for data in street_datalist:
+                if data[10].upper() == checkvalue.upper():
+                    if line[9] == data[23]:
+                        counter+=1
+    return counter   
+
 def busstops(street_datalist,bus_datalist):
     aacount=0
     nscount=0
@@ -15,25 +24,25 @@ def busstops(street_datalist,bus_datalist):
         if line[7].upper() == 'Accessible'.upper():
             for data in street_datalist:
                 if data[10].upper() == 'ARTERIAL'.upper():
-                    #print('ARTERIAL',line[9],data[23],line[9] == data[23])
                     if line[9] == data[23]:
                         aacount+=1
+            
         elif line[7].upper() == 'Non-Standard'.upper():
             for data in street_datalist:
-                if data[10].upper() == 'LOCAL STREET'.upper():
-                    #print('LOCAL',line[9],data[23],line[9] == data[23])
+                if data[10].upper() == 'LOCAL STREET'.upper(): 
                     if line[9] == data[23]:
                         nscount+=1
-        elif line[7].upper() == 'Inaccessible'.upper():
+            
+        elif line[7].upper() == 'Inaccessible'.upper():     
             for data in street_datalist:
-                if data[10].upper() == 'MINOR COLLECTOR'.upper():
-                    #print('MINOR',line[9],data[23],line[9] == data[23])
+                if data[10].upper() == 'MINOR COLLECTOR'.upper(): 
                     if line[9] == data[23]:
                         incount+=1
+            
     print('The Number of bus Stops which are Accessible and in a ARTERIAL road::',aacount)
     print('The Number of bus Stops which are Non-Standard and in a LOCAL STREET::',nscount)
     print('The Number of bus Stops which are Inaccessible and in a MINOR COLLECTOR::',incount)
-    
+
 def printsomedata(bus_datalist):
         print('|Stop Number|Location|FCODE|')
         print('|'+'---------|'*3)
@@ -45,5 +54,5 @@ fout=open('Street_Centrelines.csv','r')
 street_datalist=read_csv(fout)
 fout=open('Bus_Stops.csv','r')
 bus_datalist=read_csv(fout)
-#busstops(street_datalist,bus_datalist)
+busstops(street_datalist,bus_datalist)
 printsomedata(bus_datalist)
